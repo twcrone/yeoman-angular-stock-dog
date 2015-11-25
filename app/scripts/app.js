b3c94ff12f690435e8f2c26be466685daac19e34
Todd Crone <todd.crone@perceptivesoftware.com>
2015-11-24 22:34:08 -0500

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDogApp
+ * @description
+ * # stockDogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDogApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
